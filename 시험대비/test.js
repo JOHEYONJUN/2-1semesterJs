25f0a7302d36831efdfe69e482a3cf821869a480
NEMURENAIYORU <lhslhs301@gmail.com>
2023-04-27 11:04:53 +0900

    Docs : update JsSection

--- 시험대비/test.js
+++ 시험대비/test.js
@@ -41,3 +41,37 @@
 // $input.addEventListener('input', onInput);
 
 
+// 끝말잇기
+// 몇명 참가할지 물어보기
+const number = Number(prompt('몇명이서 하니?'));
+const $input = document.querySelector('input');
+const $word = document.querySelector('#word');
+const $button = document.querySelector('button');
+const $order = document.querySelector('#order');
+
+let word; // 제시어
+let order = 1; // 순서(1번)
+// 버튼 클릭
+$button.addEventListener('click', () => {
+    let inputValue = $input.value; // 클릭할때마다 input의 value를 저장
+    // 입력한 단어를(정확할 시) 제시어로 변경하기
+    if (!word || word[word.length - 1] === inputValue[0]) {
+        word = inputValue;
+        $word.textContent = word;
+        // 참가자를 리셋시키기
+        if (number > order) {
+            order += 1;
+            $order.textContent = order;
+        }
+        else {
+            order = 1;
+            $order.textContent = order;
+        }
+    }
+    else {
+        alert('정확하게 입력하세요.')
+    }
+    $input.value = '';
+    $input.focus();
+})
+
